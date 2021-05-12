58667a2acd067d3d3a42571aa1efd65702818cf9
zem0rts <76775477+zem0rts@users.noreply.github.com>
2021-05-12 19:56:43 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'May 13, 2021',
-  name: 'Phú'
+  birthdate: 'May 12, 2021',
+  name: 'Minh'
 };
 
 function hideEverything() {
